235c9390069af662169bfdd838f1b1962017146c
ChrissiQ <rediahs@gmail.com>
2018-04-01 23:49:03 -0600

    Rearrange some keys for efficiency/comfort. Add nav layer with arrows, pgup/pgdn, mouse, etc.

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -14,6 +14,7 @@ extern keymap_config_t keymap_config;
 #define _WORKMAN 3
 #define _LOWER 4
 #define _RAISE 5
+#define _NAV 6
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -23,6 +24,7 @@ enum custom_keycodes {
   WORKMAN,
   LOWER,
   RAISE,
+  NAV,
   ADJUST,
 };
 
@@ -88,62 +90,81 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Workman
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  |   ;  | Esc  |
+ * | Esc  |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  |   ;  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Bksp |   A  |   S  |   H  |   T  |   G  |   Y  |   N  |   E  |   O  |   I  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Tab  |   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  |Shift |
+ * | Tab  |   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  |Adjust|Lower |Enter |Space |Raise | Left | Down |  Up  |Right |
- * |      |      |      |      |      |(Shft)|      |      |      |      |      |      |
+ * | Ctrl | GUI  | Alt  |Adjust|Lower |Enter |Space |Raise | Nav  |RShift| RGUI | Caps |
+ * |      |      |      |      |      |(Shft)|      |      |      |      |      | Lock |
  * `-----------------------------------------------------------------------------------'
  */
 [_WORKMAN] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,          KC_J,   KC_F,    KC_U,    KC_P,    KC_SCLN, KC_ESC, \
-  KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,          KC_Y,   KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
-  KC_TAB,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,          KC_K,   KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  LOWER,   SFT_T(KC_ENT), KC_SPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC,  KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,          KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSLS, \
+  KC_BSPC, KC_A,    KC_S,    KC_H,    KC_T,    KC_G,          KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT, \
+  KC_TAB,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,          KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL, \
+  KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  LOWER,   SFT_T(KC_ENT), KC_SPC,  RAISE,   NAV,     KC_RSFT, KC_RGUI, KC_LCAP \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   |  |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
+ * | Bksp |      |      |      |   (  |      |      |   )  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |Enter |Space |      | Next | Vol- | Vol+ | Play |
+ * | Tab  |      |      |      |      |Enter |Space |      | Next | Vol- | Vol+ | Play |
  * |      |      |      |      |      |(Shft)|      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,        KC_F12,  S(KC_NUHS), S(KC_NUBS), _______, _______, _______, \
-  _______, _______, _______, _______, _______, SFT_T(KC_ENT), KC_SPC,  _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,      KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR,    KC_ASTR, KC_PIPE, _______, _______, \
+  KC_BSPC, _______, _______, _______, KC_LPRN, _______,       _______, KC_RPRN,    _______, _______, _______, _______, \
+  KC_TAB,  _______, _______, _______, _______, SFT_T(KC_ENT), KC_SPC,  _______,    KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Bksp |      |   _  |   +  |   {  |      |      |   }  |   -  |   =  |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
+ * | Tab  |      |      |      |   [  |      |      |   ]  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |Enter |Space |      | Home | PgDn | PgUp | End  |
+ * |      |      |      |      |      |Enter |Space |      |      |      |      |      |
  * |      |      |      |      |      |(Shft)|      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,        KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, SFT_T(KC_ENT), KC_SPC,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+  KC_BSPC, _______, KC_UNDS, KC_PLUS, KC_LCBR, _______,        _______, KC_RCBR, KC_MINS, KC_EQL,  _______, _______, \
+  KC_TAB,  _______, _______, _______, KC_LBRC, _______,        _______, KC_RBRC, _______, _______, _______, _______, \
+  _______,  _______, _______, _______, _______, SFT_T(KC_ENT), KC_SPC,  _______, _______, _______, _______, _______ \
 ),
 
-/* Adjust (Lower + Raise)
+/* Navigate
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |R Clck|L Clck|      |      |L Clck|R Clck|      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |Ms Lft|Ms Dn |Ms Up |Ms Rgt|      |      | Left | Down |  Up  |Right |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |MsW Lt|MsW Dn|MsW Up|MsW Rt|      |      | Home | PgDn | PgUp | End  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |Enter |Space |      |      |      |      |      |
+ * |      |      |      |      |      |(Shft)|      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NAV] =  KEYMAP( \
+  _______, _______, _______, KC_BTN2, KC_BTN1, _______,       _______, KC_BTN1, KC_BTN2, _______, _______, _______, \
+  _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
+  _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,       _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, \
+  _______, _______, _______, _______, _______, SFT_T(KC_ENT), KC_SPC,  _______, _______, _______, _______, _______ \
+),
+
+/* Adjust
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -155,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, AU_ON,   AU_OFF,  _______, _______, QWERTY,  COLEMAK, DVORAK,  WORKMAN, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -231,6 +252,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case NAV:
+      if (record->event.pressed) {
+        layer_on(_NAV);
+      } else {
+        layer_off(_NAV);
       }
       return false;
       break;
